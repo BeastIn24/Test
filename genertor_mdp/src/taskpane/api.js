22113f2fb32711d9fa7a87ba92389ebc36fa3405
BeastIn24 <79659410+BeastIn24@users.noreply.github.com>
2023-06-15 15:27:36 +0200

    test

---
+++ genertor_mdp/src/taskpane/api.js
@@ -0,0 +1,12 @@
+import createPW from "./pw.js";
+document.getElementById("button").onclick = function() {getPassword()};
+function getPassword() {
+  fetch("https://trouve-mot.fr/api/random/3")
+    .then((response) => response.json())
+    .then((words) => {
+      let str = words
+      let passphrase = str[0]["name"] + " " + str[1]["name"] + " " + str[2]["name"]
+      let password = createPW(passphrase)
+      console.log(password)
+    })
+}
