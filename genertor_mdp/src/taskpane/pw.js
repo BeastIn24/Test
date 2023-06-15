f5a80b6d1d355c1cd7b1b0cc3612de3484c98113
BeastIn24 <79659410+BeastIn24@users.noreply.github.com>
2023-06-15 17:14:14 +0200

    test

--- genertor_mdp/src/taskpane/pw.js
+++ genertor_mdp/src/taskpane/pw.js
@@ -1,25 +1,25 @@
 function createPW(passphrase) {
   let password = passphrase[0].toUpperCase();
-  var specialList = "`~!@#$%^&*-_=+\\|;:'\",./?";
+  var specialList = "`~!@#$%^&*-_=+\\|;:,./?";
   for (let i = 1; i < passphrase.length; i++) {
     if (passphrase[i] == 'a') {
-      password[i] = '4';
+      password = password + '4';
     }
     if (passphrase[i] == 'e') {
-      password[i] = '3';
+      password = password + '3';
     }
     if (passphrase[i] == 's') {
-      password[i] = '5';
+      password = password + '5';
     }
     if (passphrase[i] == 'l') {
-      password[i] = '1' ;
+      password = password + '1';
     }
     if (passphrase[i] == 'o') {
-      password[i] = '0'; 
+      password = password + '0';
     }
     if (passphrase[i] == ' ') {
-      password[i] = speciallist[Math.floor(Math.random() * list.length)];
-      password[i+1] = passphrase[i+1].toUpperCase();
+      password = password + specialList[Math.floor(Math.random() * list.length)];
+      password = password + passphrase[i+1].toUpperCase();
       i++;
     }
   }
