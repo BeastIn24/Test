3d06afcda2bbf88f3e8c481a02d06782f29b49db
BeastIn24 <79659410+BeastIn24@users.noreply.github.com>
2023-06-16 10:48:42 +0200

    test

--- genertor_mdp/src/taskpane/taskpane.js
+++ genertor_mdp/src/taskpane/taskpane.js
@@ -14,17 +14,4 @@ Office.onReady((info) => {
 });
 
 export async function run() {
-  console.log(test);
-  import createPW from "./pw.js";
-  document.getElementById("button").onclick = function() {getPassword()};
-  function getPassword() {
-    fetch("https://trouve-mot.fr/api/size/3/3")
-      .then((response) => response.json())
-      .then((words) => {
-        let str = words
-        let passphrase = str[0]["name"] + " " + str[1]["name"] + " " + str[2]["name"]
-        let password = createPW(passphrase)
-        document.getElementById("password").value = password
-      })
-  }
 }
