ffc41d1e8a9764807d650406d446f63857f538d8
BeastIn24 <79659410+BeastIn24@users.noreply.github.com>
2023-07-13 17:38:01 +0200

    test

--- Taskpane/src/taskpane/pw.js
+++ Taskpane/src/taskpane/pw.js
@@ -1,41 +1,40 @@
 function createPW(passphrase) {
-  let password = passphrase[0].toUpperCase();
   var specialList = "`~!@#$%^&*-_=+\\|;:,./?";
   var list = ""
-  for (let i = 1; i < passphrase.length; i++) {
+  for (let i = 0; i < passphrase.length; i++) {
     if (passphrase[i] == 'a' || passphrase[i] == 'à' || passphrase[i] == 'â') {
-      list = "aaaa44A";
+      list = "aaaaaa44A";
       password = password + list[Math.floor(Math.random() * list.length)];
     }
     else if (passphrase[i] == 'e' || passphrase[i] == 'é' || passphrase[i] == 'è' || passphrase[i] == 'ê') {
-      list = "eeee33E";
+      list = "eeeeee33E";
       password = password + list[Math.floor(Math.random() * list.length)];
     }
     else if (passphrase[i] == 's') {
-      list = "ssss55S";
+      list = "ssssss55S";
       password = password + list[Math.floor(Math.random() * list.length)];
     }
     else if (passphrase[i] == 'l') {
-      list = "llll11L";
+      list = "llllll11L";
       password = password + list[Math.floor(Math.random() * list.length)];
     }
     else if (passphrase[i] == 'o' || passphrase[i] == 'ô') {
-      list = "oooo00O";
+      list = "oooooo00O";
       password = password + list[Math.floor(Math.random() * list.length)];
     }
     else if (passphrase[i] == ' ') {
       password = password + specialList[Math.floor(Math.random() * specialList.length)];
     }
     else if (passphrase[i] == 'ù' || passphrase[i] == 'û') {
-      list = "uuuuU";
+      list = "uuuuuuU";
       password = password + list[Math.floor(Math.random() * list.length)];
     }
     else if (passphrase[i] == 'î' || passphrase[i] == 'ï') {
-      list = "iiiiI";
+      list = "iiiiiiI";
       password = password + list[Math.floor(Math.random() * list.length)];
     }
     else {
-      let n = Math.floor(Math.random() * 4);
+      let n = Math.floor(Math.random() * 8);
       if (n == 0) {
         password = password + passphrase[i].toUpperCase();
       }
