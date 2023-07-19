4d42126dbef2c7f1cafc49ba3d9dd33aa5016d75
BastienDfx <bdefourneaux@pragmatic-conseil.fr>
2023-07-19 15:24:54 +0200

    V2

--- Taskpane/src/taskpane/pw.js
+++ Taskpane/src/taskpane/pw.js
@@ -3,47 +3,47 @@ function createPW(passphrase) {
   var list = "";
   var password ="";
   for (let i = 0; i < passphrase.length; i++) {
-    if (passphrase[i] == 'a' || passphrase[i] == 'à' || passphrase[i] == 'â') {
-      list = "aaaaaa44A";
+  if (passphrase[i] == 'a' || passphrase[i] == 'à' || passphrase[i] == 'â') {
+      list = "aaaaa44A";
       password = password + list[Math.floor(Math.random() * list.length)];
-    }
-    else if (passphrase[i] == 'e' || passphrase[i] == 'é' || passphrase[i] == 'è' || passphrase[i] == 'ê') {
-      list = "eeeeee33E";
+  }
+  else if (passphrase[i] == 'e' || passphrase[i] == 'é' || passphrase[i] == 'è' || passphrase[i] == 'ê') {
+      list = "eeeee33E";
       password = password + list[Math.floor(Math.random() * list.length)];
-    }
-    else if (passphrase[i] == 's') {
-      list = "ssssss55S";
+  }
+  else if (passphrase[i] == 's') {
+      list = "sssss55S";
       password = password + list[Math.floor(Math.random() * list.length)];
-    }
-    else if (passphrase[i] == 'l') {
-      list = "llllll11L";
+  }
+  else if (passphrase[i] == 'l') {
+      list = "lllll11L";
       password = password + list[Math.floor(Math.random() * list.length)];
-    }
-    else if (passphrase[i] == 'o' || passphrase[i] == 'ô') {
-      list = "oooooo00O";
+  }
+  else if (passphrase[i] == 'o' || passphrase[i] == 'ô') {
+      list = "ooooo00O";
       password = password + list[Math.floor(Math.random() * list.length)];
-    }
-    else if (passphrase[i] == ' ') {
+  }
+  else if (passphrase[i] == ' ') {
       password = password + specialList[Math.floor(Math.random() * specialList.length)];
-    }
-    else if (passphrase[i] == 'ù' || passphrase[i] == 'û') {
-      list = "uuuuuuU";
+  }
+  else if (passphrase[i] == 'ù' || passphrase[i] == 'û') {
+      list = "uuuuuU";
       password = password + list[Math.floor(Math.random() * list.length)];
-    }
-    else if (passphrase[i] == 'î' || passphrase[i] == 'ï') {
-      list = "iiiiiiI";
+  }
+  else if (passphrase[i] == 'î' || passphrase[i] == 'ï') {
+      list = "iiiiiI";
       password = password + list[Math.floor(Math.random() * list.length)];
-    }
-    else {
+  }
+  else {
       let n = Math.floor(Math.random() * 8);
       if (n == 0) {
-        password = password + passphrase[i].toUpperCase();
+      password = password + passphrase[i].toUpperCase();
       }
       else {
-        password = password + passphrase[i];
+      password = password + passphrase[i];
       }
       
-    }
+  }
   }
   return password;
 }
--- Taskpane/src/taskpane/taskpane.js
+++ Taskpane/src/taskpane/taskpane.js
@@ -1,54 +1,60 @@
 import {createPW} from "./pw.js";
-document.getElementById("genPwButton").onclick = function() {getPassword(12)};
-function getPassword(n) {
-  var request = "https://trouve-mot.fr/api/size/";
-  let passphrase = "";
-  if (n <= 13) {
-    var a = Math.floor(Math.random() * (n-4))+2;
-    var b = n-a-1;
-    var request1 = request + a;
-    var request2 = request + b;
-    fetch(request1.toString())
-    .then((response) => response.json())
-    .then((words) => {
-      let str = words;
-      var word = str[0]["name"];
-      passphrase = passphrase  + word;
-    })
-  }
-  else {
-    var a = Math.floor(Math.random() * (n-7))+2;
-    var b = Math.floor(Math.random() * (n-a-5))+2;
-    var c = n-a-b-2;
-    var request1 = request + a;
-    var request2 = request + b;
-    var request3 = request + c;
-    passphrase = passphrase  + getWord(request1) + " " + getWord(request2) + " " + getWord(request3);
-  }
-  let password = createPW(passphrase);
-  document.getElementById("password").value = passphrase;
+document.getElementById("genPwButton").onclick = function() {getPassword(document.getElementById("length").value)};
+async function getPassword(n) {
+    let request = "https://trouve-mot.fr/api/size/";
+    let passphrase = "";
+    let word1 = "";
+    let word2 = "";
+    let word3 = "";
+    let a;
+    let b;
+    let c;
+    let request1;
+    let request2;
+    let request3;
+    if (n <= 13) {
+        a = Math.floor(Math.random() * (n-4))+2;
+        b = n-a-1;
+        request1 = request + a;
+        word1 = await getWord(request1);
+        console.log("word1 = " + word1);
+        request2 = request + b;
+        word2 = await getWord(request2);
+        passphrase = passphrase + word1 + " " + word2;
+    }
+    else {
+        a = Math.floor(Math.random() * (n-7))+2;
+        b = Math.floor(Math.random() * (n-a-5))+2;
+        c = n-a-b-2;
+        request1 = request + a;
+        word1 = await getWord(request1);
+        request2 = request + b;
+        word2 = await getWord(request2);
+        request3 = request + c;
+        word3 = await getWord(request3);
+        passphrase = passphrase  + word1 + " " + word2 + " " + word3;
+    }
+    let password = createPW(passphrase);
+    document.getElementById("password").value = password;
 }
 
-function getWord(request) {
-  fetch(request.toString())
-    .then((response) => response.json())
-    .then((words) => {
-      let str = words;
-      return(str[0]["name"]);
-    })
+async function getWord(request) {
+const result = await fetch(request)
+const words = await result.json()
+return words[0]["name"];
 }
 
 document.getElementById("genPwButton2").onclick = function() {getPasswordfromUser()};
 function getPasswordfromUser() {
-  let w1 = document.getElementById("word1").value;
-  let w2 = document.getElementById("word2").value;
-  let w3 = document.getElementById("word3").value;
-  let word1 = w1.toString();
-  let word2 = w2.toString();
-  let word3 = w3.toString();
-  let passphrase = word1 + " " + word2 + " " + word3;
-  let password = createPW(passphrase);
-  document.getElementById("password2").value = password;
+let w1 = document.getElementById("word1").value;
+let w2 = document.getElementById("word2").value;
+let w3 = document.getElementById("word3").value;
+let word1 = w1.toString();
+let word2 = w2.toString();
+let word3 = w3.toString();
+let passphrase = word1 + " " + word2 + " " + word3;
+let password = createPW(passphrase);
+document.getElementById("password2").value = password;
 
 }
 document.getElementById("copyButton").onclick = function() {copy(1)};
@@ -56,20 +62,20 @@ document.getElementById("copyButton2").onclick = function() {copy(2)};
 let pwAttr = "";
 let copyAttr = "";
 function copy(n) {
-  if (n == 1) {
+if (n == 1) {
     pwAttr = "password";
     copyAttr = "copyimg";
-  }
-  else {
+}
+else {
     pwAttr = "password2";
     copyAttr = "copyimg2";
-  }
-  var pw = document.getElementById(pwAttr);
-  pw.select();
-  navigator.clipboard.writeText(pw.value);
-  pw.blur();
-  document.getElementById(copyAttr).setAttribute("src","../../assets/checkimg.png");  
-  setTimeout(function(){ 
+}
+var pw = document.getElementById(pwAttr);
+pw.select();
+navigator.clipboard.writeText(pw.value);
+pw.blur();
+document.getElementById(copyAttr).setAttribute("src","../../assets/checkimg.png");  
+setTimeout(function(){ 
     document.getElementById(copyAttr).setAttribute("src","../../assets/copyimg.png"); 
-  }, 1000);
+}, 1000);
 }
