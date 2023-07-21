1b08b702d6670a6b4cfa4199e1fea4f9bfa6255f
BastienDfx <bdefourneaux@pragmatic-conseil.fr>
2023-07-21 16:17:13 +0200

    checkbox

--- Taskpane/src/taskpane/taskpane.js
+++ Taskpane/src/taskpane/taskpane.js
@@ -46,6 +46,7 @@ async function getPassword(n) {
     let password = createPW(passphrase);
     document.getElementById("usedwords").value = passphrase;
     document.getElementById("password").value = password;
+    updatepassword(1);
 }
 
 async function getWord(request) {
@@ -56,17 +57,83 @@ return words[0]["name"];
 
 document.getElementById("genPwButton2").onclick = function() {getPasswordfromUser()};
 function getPasswordfromUser() {
-let w1 = document.getElementById("word1").value;
-let w2 = document.getElementById("word2").value;
-let w3 = document.getElementById("word3").value;
-let word1 = w1.toString();
-let word2 = w2.toString();
-let word3 = w3.toString();
-let passphrase = word1 + " " + word2 + " " + word3;
-let password = createPW(passphrase);
-document.getElementById("password2").value = password;
-
+    let w1 = document.getElementById("word1").value;
+    let w2 = document.getElementById("word2").value;
+    let w3 = document.getElementById("word3").value;
+    let word1 = w1.toString();
+    let word2 = w2.toString();
+    let word3 = w3.toString();
+    let passphrase = word1 + " " + word2 + " " + word3;
+    let password = createPW(passphrase);
+    document.getElementById("password2").value = password;
+    updatepassword(2);
 }
+
+function updatepassword(i) {
+    let password;
+    if (i == 1) {
+        document.getElementById("password2").value = "";
+        password = document.getElementById("password");
+        checkpassword(password);
+    }
+    else {
+        document.getElementById("password").value = "";
+        password = document.getElementById("password2");
+        checkpassword(password);
+    }
+}
+
+function checkpassword(password) {
+    let len, lower, upper, special, number = false;
+    if (length(password) >= 8) {
+        len = true;
+    }
+    for (i=0; i<length(password); i++) {
+        if (97 <= password.charCodeAt(i) <= 122) {
+            lower = true;
+        }
+        if (65 <= password.charCodeAt(i) <= 90) {
+            upper = true;
+        }
+        if (33 <= password.charCodeAt(i) <= 47 || 58 <= password.charCodeAt(i) <= 64 || 91 <= password.charCodeAt(i) <= 96) {
+            special = true;
+        }
+        if (48 <= password.charCodeAt(i) <= 57) {
+            number = true;
+        }
+    }
+    if (len) {
+        document.getElementById('checklen').style.color = green;
+    }
+    else {
+        document.getElementById('checklen').style.color = red;
+    }
+    if (len) {
+        document.getElementById('checklower').style.color = green;
+    }
+    else {
+        document.getElementById('checklower').style.color = red;
+    }
+    if (len) {
+        document.getElementById('checkupper').style.color = green;
+    }
+    else {
+        document.getElementById('checkupper').style.color = red;
+    }
+    if (len) {
+        document.getElementById('checkspecial').style.color = green;
+    }
+    else {
+        document.getElementById('checkspecial').style.color = red;
+    }
+    if (len) {
+        document.getElementById('checknumber').style.color = green;
+    }
+    else {
+        document.getElementById('checknumber').style.color = red;
+    }
+}
+
 document.getElementById("copyButton").onclick = function() {copy(1)};
 document.getElementById("copyButton2").onclick = function() {copy(2)};
 let pwAttr = "";
